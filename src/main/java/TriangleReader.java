e041505443ef258c09b1336dbb26e1a6b17c4562
Jake K <68135816+Sylk0s@users.noreply.github.com>
2021-06-19 13:13:25 -0400

    Spits something out... its just wrong

--- src/main/java/TriangleReader.java
+++ src/main/java/TriangleReader.java
@@ -44,6 +44,7 @@ public class TriangleReader {
             HashMap<Integer, Triangle> perimeters = new HashMap<>();
             HashMap<Integer, List<Triangle>> perimeterresults  = new HashMap<>();
             HashMap<Integer, List<Triangle>> arearesults = new HashMap<>();
+            List<List<Triangle>> finalresults = new ArrayList<>();
 
             for(Triangle triangle : triangles) {
                 int perimeter = triangle.P;
@@ -76,12 +77,45 @@ public class TriangleReader {
                     arearesults.put(peri, samearea);
             });
 
-            System.out.println(arearesults.size());
+            arearesults.forEach((a,t) -> {
+                Triangle ref = t.get(0);
+                    for(Triangle triangle : getPermutations(ref)) {
+                        float t1Cos = (float) ((Math.pow(triangle.a3, 2) - Math.pow(triangle.a1, 2) - Math.pow(triangle.a2, 2)) / (-2 * triangle.a1 * triangle.a2));
+                        for(int index = 1; index < t.size(); index++) {
+                            Triangle triangle2 = t.get(index);
+                            float t2Cos = (float) ((Math.pow(triangle2.a3, 2) - Math.pow(triangle2.a1, 2) - Math.pow(triangle2.a2, 2)) / (-2 * triangle2.a1 * triangle2.a2));
+                            if(roughlyEqualsNegative(t1Cos, t2Cos)) {
+                                List<Triangle> result = new ArrayList<>();
+                                result.add(triangle);
+                                result.add(triangle2);
+                                finalresults.add(result);
+                            }
+                        }
+                    }
+            });
 
             File file = new File("output.txt");
             file.createNewFile();
             FileWriter writer = new FileWriter(file.getPath());
 
+            finalresults.forEach((result) -> {
+                try {
+                    writer.write("Pair: \n");
+                    result.forEach(t -> {
+                        try {
+                            writer.write(t.a1 + ", " + t.a2 + ", " + t.a3 + "\n");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    });
+                    writer.write(" \n");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            });
+            writer.close();
+
+            /*
             arearesults.forEach((p, a) -> {
                 try {
                     writer.write("With area: " + a.get(0).D + "\n");
@@ -97,17 +131,49 @@ public class TriangleReader {
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
-                //System.out.println("With area: " + a.get(0).D);
-                //System.out.println("With perimeter: " + a.get(0).P);
-                //a.forEach(t -> {
-                //System.out.println(t.a1 + ", " + t.a2 + ", " + t.a3);
-                //});
             });
 
             writer.close();
+            */
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+    }
+
+    public static Triangle[] getPermutations(Triangle triangle) {
+        Triangle t1 = new Triangle();
+        t1.a1 = triangle.a1;
+        t1.a2 = triangle.a2;
+        t1.a3 = triangle.a3;
+        Triangle t2 = new Triangle();
+        t2.a1 = triangle.a1;
+        t2.a2 = triangle.a3;
+        t2.a3 = triangle.a2;
+        Triangle t3 = new Triangle();
+        t3.a1 = triangle.a2;
+        t3.a2 = triangle.a1;
+        t3.a3 = triangle.a3;
+        Triangle t4 = new Triangle();
+        t4.a1 = triangle.a2;
+        t4.a2 = triangle.a3;
+        t4.a3 = triangle.a1;
+        Triangle t5 = new Triangle();
+        t5.a1 = triangle.a3;
+        t5.a2 = triangle.a1;
+        t5.a3 = triangle.a2;
+        Triangle t6 = new Triangle();
+        t6.a1 = triangle.a3;
+        t6.a2 = triangle.a2;
+        t6.a3 = triangle.a1;
+
+        return new Triangle[]{t1, t2, t3, t4, t5, t6};
+    }
+
+    public static final float ERROR_AMOUNT = 0.1f;
+
+    public static boolean roughlyEqualsNegative(float num, float num2) {
+        if(Math.abs(num - num2) < ERROR_AMOUNT) return true;
+        else return false;
     }
 }
